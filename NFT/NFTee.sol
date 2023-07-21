942d216963d07838bc21f2801778a5b8e00a44d0
Rishabh Singh <mrrobot@Rishabhs-MacBook-Air.local>
2023-07-22 01:53:46 +0530

    NFTv1

--- NFT/NFTee.sol
+++ NFT/NFTee.sol
@@ -1,9 +1,10 @@
-pragama solidity ^0.8.18;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFTee is ERC721 {
-    constructor() ERC721("NFTee","ITM"){
-        mint(msg.sender,1);
+    constructor() ERC721("NFTee", "ITM") {
+        // mint an NFT to yourself
+        _mint(msg.sender, 1);
     }
 }
